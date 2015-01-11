bd175a8ca15e89d219ddf1fac4ebe40ebe1b4dd8
Eugene Kochetov <ekochetov@yandex.ru>
2015-01-11 20:26:44 +0300

    Added first tests with expected mock method calls

--- UnitTestingDemoApp/test/unit/phonerecordtest.cpp
+++ UnitTestingDemoApp/test/unit/phonerecordtest.cpp
@@ -2,6 +2,9 @@
 #include "gmock/gmock.h"
 #include "phonerecord.h"
 
+using ::testing::_;
+using ::testing::Ref;
+
 class MockDataProvider : public DataProvider
 {
     // DataProvider interface
@@ -44,5 +47,45 @@ TEST(phoneRecordTest, contructor_WithDataProviderAndValues_FieldsNotEmpty)
     EXPECT_EQ("John", record->getFirstName());
     EXPECT_EQ("Lock", record->getLastName());
     EXPECT_EQ("4815162342", record->getPhone());
+}
+
+TEST(phoneRecordTest, save_NewRecord_RecordIsNotNew)
+{
+    MockDataProvider dataProvider;
+    PhoneRecord *record = new PhoneRecord(dataProvider);
+    record->save();
+    EXPECT_FALSE(record->isNew());
+}
+
+TEST(phoneRecordTest, save_NewRecord_AddPhoneRecordIsCalled)
+{
+    MockDataProvider dataProvider;
+    PhoneRecord *record = new PhoneRecord(dataProvider);
+    EXPECT_CALL(dataProvider, addPhoneRecord(_));
+    record->save();
+}
+
+TEST(phoneRecordTest, save_NewRecord_AddPhoneRecordIsCalledWithTestedRecord)
+{
+    MockDataProvider dataProvider;
+    PhoneRecord *record = new PhoneRecord(dataProvider);
+    EXPECT_CALL(dataProvider, addPhoneRecord(Ref(*record)));
+    record->save();
+}
+
+TEST(phoneRecordTest, save_NotNewRecord_SavePhoneRecordIsCalled)
+{
+    MockDataProvider dataProvider;
+    PhoneRecord *record = new PhoneRecord(dataProvider, "John", "Lock", "4815162342");
+    EXPECT_CALL(dataProvider, savePhoneRecord(_));
+    record->save();
+}
+
+TEST(phoneRecordTest, save_NotNewRecord_SavePhoneRecordIsCalledWithTestedRecord)
+{
+    MockDataProvider dataProvider;
+    PhoneRecord *record = new PhoneRecord(dataProvider, "John", "Lock", "4815162342");
+    EXPECT_CALL(dataProvider, savePhoneRecord(Ref(*record)));
+    record->save();
 }
 
